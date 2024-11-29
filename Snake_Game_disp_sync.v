7b543e0d1f91e9513f068629822102b652eefc44
Anirudh Gupta <ganirudh118@gmail.com>
2024-11-29 17:13:31 +0530

    Update Snake_Game_disp_sync.v

--- Snake_Game_disp_sync.v
+++ Snake_Game_disp_sync.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+timescale 1ns / 1ps
 
 module disp_sync(
   input clk, rst,
@@ -9,54 +9,57 @@ module disp_sync(
   
   always @(posedge clk) begin    
   
-	  if(rst)
-	    h_loc <= 11'b00000000001;
-	  else if(h_loc >= 11'b10000100000)			
-        h_loc <= 11'b0000000001;
-      else
-        h_loc <= h_loc + 11'b00000000001;
-	  //counter for horizontal position.
+    // Horizontal counter
+    if(rst)
+      h_loc <= 11'b00000000001;
+    else if(h_loc >= 11'b11001000000)  // 800, end of horizontal line
+      h_loc <= 11'b0000000001;  // Reset to start of line
+    else
+      h_loc <= h_loc + 11'b00000000001;  // Increment for horizontal position
       
-	  if(rst)
-		v_loc <= 10'b0000000001;
-      else if(h_loc == 11'b10000100000 && v_loc >= 10'b1001110100)			
-        v_loc <= 10'b0000000001;
-      else if(h_loc == 11'b10000100000)
-        v_loc <= v_loc + 10'b0000000001;
-      else
-        v_loc <= v_loc;
-      //counter for vertical position.
-	  
-	  if(rst)
-		h_sync <= 1'b0;
-      if(h_loc == 11'b01101001000)
-		h_sync <= 1'b1;
-      if(h_loc == 11'b1111001000)
-		h_sync <= 1'b0;
-	  //if block for horizontal sync pulse
-	  
-	  if(rst)
-		v_sync <= 1'b0;
-      if(v_loc == 10'b1001011001)
-		v_sync <= 1'b1;
-      if(v_loc == 10'b1001011101)
-		v_sync <= 1'b0;
-	  //if block for vertical sync pulse
-	  
-	  if(rst)
-		h_disp <= 1'b1;
-      if(h_loc == 11'b01100100000)
-		h_disp <= 1'b0;
-      if(h_loc == 11'b10000100000)
-		h_disp <= 1'b1;
-	  
-	  if(rst)
-		v_disp <= 1'b1;
-      if(v_loc == 10'b1001010111)
-		v_disp <= 1'b0;
-      if(v_loc == 10'b1001110100)
-		v_disp <= 1'b1;
-		
-      end
+    // Vertical counter
+    if(rst)
+      v_loc <= 10'b0000000001;
+    else if(h_loc == 11'b11001000000 && v_loc >= 10'b1000000000)  // 525, end of frame
+      v_loc <= 10'b0000000001;  // Reset to start of frame
+    else if(h_loc == 11'b11001000000)  // End of one horizontal line
+      v_loc <= v_loc + 10'b0000000001;  // Increment for vertical position
+    else
+      v_loc <= v_loc;
+  
+    // Horizontal sync signal (active low, 96 cycles)
+    if(rst)
+      h_sync <= 1'b0;
+    else if(h_loc == 11'b1010010000)  // 656, start of sync pulse
+      h_sync <= 1'b1;
+    else if(h_loc == 11'b1011111000)  // 752, end of sync pulse
+      h_sync <= 1'b0;
+    
+    // Vertical sync signal (active low, 2 lines)
+    if(rst)
+      v_sync <= 1'b0;
+    else if(v_loc == 10'b1111011011)  // 491, start of vertical sync pulse
+      v_sync <= 1'b1;
+    else if(v_loc == 10'b1111011111)  // 493, end of vertical sync pulse
+      v_sync <= 1'b0;
+    
+    // Horizontal display enable signal (active high, 640 pixels)
+    if(rst)
+      h_disp <= 1'b1;
+    else if(h_loc == 11'b10100010000)  // 640, end of visible area
+      h_disp <= 1'b0;
+    else if(h_loc == 11'b0000000001)  // 1, start of visible area
+      h_disp <= 1'b1;
+  
+    // Vertical display enable signal (active high, 480 lines)
+    if(rst)
+      v_disp <= 1'b1;
+    else if(v_loc == 10'b1111001000)  // 480, end of visible area
+      v_disp <= 1'b0;
+    else if(v_loc == 10'b0000000001)  // 1, start of visible area
+      v_disp <= 1'b1;
+
+  end
   
 endmodule
+
